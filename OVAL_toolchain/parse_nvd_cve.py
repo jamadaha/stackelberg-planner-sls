5eac57a0f9ec2040bd3779e4c3850842b4b3e73d
Patrick Speicher <s9pkspei@stud.uni-saarland.de>
2017-01-17 10:13:18 +0100

    parse multiple mdv files into on son dictionary

--- OVAL_toolchain/parse_nvd_cve.py
+++ OVAL_toolchain/parse_nvd_cve.py
@@ -37,34 +37,35 @@ class MyEncoder(json.JSONEncoder):
 
         return json.dumps(obj.__dict__)
 
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
-for entry in root:
-    print(entry.tag, entry.attrib)
-    id = entry.attrib['id']
-    for entryChild in entry:
-        print(entryChild.tag, entryChild.attrib)
-        if entryChild.tag == '{http://scap.nist.gov/schema/vulnerability/0.4}cvss':
-            cvss_base_metrics = entryChild[0]
-            for base_metric in cvss_base_metrics:
-                print(base_metric.tag, base_metric.attrib)
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}score':
-                    score = base_metric.text
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}access-vector':
-                    access_vector = base_metric.text
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}access-complexity':
-                    access_complexity = base_metric.text
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}authentication':
-                    authentication = base_metric.text
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}confidentiality-impact':
-                    confidentiality_impact = base_metric.text
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}integrity-impact':
-                    integrity_impact = base_metric.text
-                if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}availability-impact':
-                    availability_impact = base_metric.text
-            entry = Entry(id, score, access_vector, access_complexity, authentication, confidentiality_impact, integrity_impact, availability_impact)
-            entries[id] = entry
+for i in range(1, len(sys.argv)):
+    tree = ET.parse(sys.argv[i])
+    root = tree.getroot()
+    for entry in root:
+        print(entry.tag, entry.attrib)
+        id = entry.attrib['id']
+        for entryChild in entry:
+            print(entryChild.tag, entryChild.attrib)
+            if entryChild.tag == '{http://scap.nist.gov/schema/vulnerability/0.4}cvss':
+                cvss_base_metrics = entryChild[0]
+                for base_metric in cvss_base_metrics:
+                    print(base_metric.tag, base_metric.attrib)
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}score':
+                        score = base_metric.text
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}access-vector':
+                        access_vector = base_metric.text
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}access-complexity':
+                        access_complexity = base_metric.text
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}authentication':
+                        authentication = base_metric.text
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}confidentiality-impact':
+                        confidentiality_impact = base_metric.text
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}integrity-impact':
+                        integrity_impact = base_metric.text
+                    if base_metric.tag == '{http://scap.nist.gov/schema/cvss-v2/0.2}availability-impact':
+                       availability_impact = base_metric.text
+                entry = Entry(id, score, access_vector, access_complexity, authentication, confidentiality_impact, integrity_impact, availability_impact)
+                entries[id] = entry
 
-with open("nvd.json", "a") as myfile:
+with open("nvd.json", "w") as myfile:
     myfile.write(json.dumps(entries, cls=MyEncoder))
 
