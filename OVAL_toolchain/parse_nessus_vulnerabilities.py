d332dc5fe10452a3029d4dcdaa1c6cef537a916e
Patrick Speicher <s9pkspei@stud.uni-saarland.de>
2017-01-17 15:17:15 +0100

    generate PDDL files after parsing Nessus results

--- OVAL_toolchain/parse_nessus_vulnerabilities.py
+++ OVAL_toolchain/parse_nessus_vulnerabilities.py
@@ -1,35 +1,175 @@
+import argparse
 import xml.etree.ElementTree as ET
 import json
 import sys
+from fractions import Fraction
+import math
 
+DIGITS = 2
+DENOMINATOR = int(math.pow(10, DIGITS))
+
+
+def round_float(f):
+    return float(("%%.%df" % DIGITS) % f)
 
 vulnerabilities = []
+hosts = ["internet"]
+uniq_ports = []
+
+
+class PDDLDomain(object):
+    def __init__(self, name, vulnerabilities, goal, uniq_ports, fixes = [], comment = None, apply_once=True):
+        self.name = name
+        self.vulnerabilities = vulnerabilities
+        self.comment = comment
+        self.goal = goal
+        self.uniq_ports = uniq_ports
+        self.fixes = fixes
+        self.apply_once = apply_once
+    def __str__(self):
+        res = ''
+        if self.comment != None:
+            res += ';;; %s\n' % self.comment
+        res += "(define (domain %s)\n" % self.name
+        res += "; (:requirements strips)\n"
+        res += "(:types host port vul"
+        res += ")\n"
+        res += "(:predicates\n"
+        res += " " * 3
+        for i in range(len(self.goal)):
+            res += " (target%d_under_control)" % i
+        res += "\n"
+        res += " " * 4 + "(controlling ?h - host)\n"
+        res += " " * 4 + "(connected ?src ?dest - host ?p - port)\n"
+        res += " " * 4 + "(vuln_fixed ?h - host ?v - vuln)\n"
+        if self.apply_once:
+            res += " " * 4 + "(applied ?h - host ?v - vul)\n"
+        res += ")\n"
+        res += "(:functions (total-cost))\n"
+        res += "(:constants\n"
+        res += " " * 4
+        for i in range(0, len(hosts)):
+            res += "%s " % hosts[i]
+        res += "- host\n"
+        res += " " * 4
+        for p in self.uniq_ports:
+            res += "p%d " % p
+        res += "pnone - port\n"
+        res += " " * 4
+        uniq = []
+        for vul in self.vulnerabilities:
+            if not vul in uniq:
+                uniq.append(vul)
+                res += "%s " % vul.id
+        res += "- vul\n"
+        res += ")\n"
+        for i in range(len(self.goal)):
+            for host in self.goal[i]:
+                res += "(:action ATTACK_set_target%d_under_control_%s_1/1\n" % (i, host)
+                res += " " * 4 + ":parameters ()\n"
+                res += " " * 4 + ":precondition (and (not (target%d_under_control)) (controlling %s))\n" % (i, host)
+                res += " " * 4 + ":effect (and (target%d_under_control) (increase (total-cost) 0))\n" % i
+                res += ")\n"
+        for vul in self.vulnerabilities:
+            f = Fraction(round_float(vul.prob)).limit_denominator(DENOMINATOR)
+            res += "(:action ATTACK_exploit_%s_%s\n" % (vul.id, f)
+            res += " " * 4 + ":parameters (?src - host ?t - host)\n"
+            res += " " * 4 + ":precondition (and (controlling ?src)"
+            res += " (connected ?src ?t p{0})".format(vul.port)
+            res += " (not (vuln_fixed ?t %s))" % vul.id
+
+            if self.apply_once:
+                res += " (not (applied ?t %s))" % vul.id
+            res += ")\n"
+            res += " " * 4 + ":effect (and (increase (total-cost) %d) (controlling ?t)" % vul.cost
+            if self.apply_once:
+                res += " (applied ?t %s)" % vul.id
+            res += ")\n"
+            res += ")\n"
+        fix_action_scheme = 0
+        for vul in self.vulnerabilities:
+            initial_cost = 0
+            cost = 1
+            res += "(:action FIX_exploit_%s_%s_%d#%d\n" % (vul.id, vul.host, initial_cost, fix_action_scheme)
+            res += " " * 4 + ":parameters (?t - host)\n"
+            res += " " * 4 + ":precondition (and (not (vuln_fixed ?t %s))" % vul.id
+            res += ")\n"
+            res += " " * 4 + ":effect (and (increase (total-cost) %d) (vuln_fixed ?t %s)" % (cost, vul.id)
+            res += ")\n"
+            res += ")\n"
+            fix_action_scheme += 1
+        res += ")\n"
+        return res
+
+class PDDLProblem(object):
+    def __init__(self, name, domain_name, controlled, goal, comment = None):
+        self.name = name
+        self.domain_name = domain_name
+        self.controlled = controlled
+        self.goal = goal
+        self.comment = comment
+    def __str__(self):
+        res = ''
+        if self.comment != None:
+            res += ";;; %s\n" % self.comment
+        res += "(define (problem %s)\n"  % self.name
+        res += "(:domain %s)\n" % self.domain_name
+        res += "(:init\n"
+        res += " " * 4 + "(= (total-cost) 0)\n"
+        res += " " * 4 + "(controlling %s)\n" % self.controlled
+        with open(args.net) as network_topology_file:
+            res += network_topology_file.read()
+        res += ")\n"
+        res += "(:goal (and\n"
+        for i in range(len(self.goal)):
+            res += " " * 4 + "(target%d_under_control)\n" % i
+        res += "))\n"
+        res += "(:metric minimize (total-cost))\n"
+        res += ")\n"
+        return res
 
 
 class Vulnerability(object):
+    id = "vul0"
     host = ""
     severity = 1
     port = 0
     risk_factor = "None"
     CVEs = []
+    prob = 0
+    cost = 0
 
     # The class "constructor" - It's actually an initializer
-    def __init__(self, host, severity, port, risk_factor, CVEs):
+    def __init__(self, id, host, severity, port, risk_factor, CVEs, prob, cost):
+        self.id = id
         self.host = host
         self.severity = severity
         self.port = port
         self.risk_factor = risk_factor
         self.CVEs = CVEs
+        self.prob = prob
+        self.cost = cost
 
     def __str__(self):
         return json.dumps(self.__dict__)
 
 
-with open(sys.argv[2]) as nvd_file:
+p = argparse.ArgumentParser(description="Nessus CoreSec Problem Generator")
+p.add_argument("--nessus", type=str, help="the Nessus output file in xml format", default=None)
+p.add_argument("--nvd", type=str, help="NVD vulnerability DB in json format", default=None)
+p.add_argument("--net", type=str, help="The network topology in PDDL format", default=None)
+p.add_argument("--disable-apply-once", help="disable apply once constraint", action="store_true", default=False)
+p.add_argument("--domain", type=str, help="path to file where the PDDL domain will be stored", default="domain.pddl")
+p.add_argument("--problem", type=str, help="path to file where the PDDL problem will be stored", default="problem.pddl")
+args = p.parse_args(sys.argv[1:])
+
+
+with open(args.nvd) as nvd_file:
    nvd_dict = json.load(nvd_file)
 
 
-tree = ET.parse(sys.argv[1])
+vuln_id = 0
+tree = ET.parse(args.nessus)
 root = tree.getroot()
 for child in root:
     print(child.tag, child.attrib)
@@ -37,12 +177,13 @@ report = root[1]
 for reportHost in report:
     print(reportHost.tag, reportHost.attrib)
     name = reportHost.attrib['name']
+    hosts.append(name)
     for reportItem in reportHost:
         print(reportItem.tag, reportItem.attrib)
         if (reportItem.tag == 'ReportItem'):
             protocol = reportItem.attrib['protocol']
-            severity = reportItem.attrib['severity']
-            port = reportItem.attrib['port']
+            severity = int(reportItem.attrib['severity'])
+            port = int(reportItem.attrib['port'])
             CVEs = []
             for reportItemChild in reportItem:
                 print(reportItemChild.tag, reportItemChild.attrib)
@@ -50,9 +191,28 @@ for reportHost in report:
                     risk_factor = reportItemChild.text
                 if (reportItemChild.tag == 'cve'):
                     CVEs.append(reportItemChild.text)
-            if (int(severity) > 0 and len(CVEs) > 0):
-                vuln = Vulnerability(name, severity, port, risk_factor, CVEs)
-                print('bla')
+            if (severity > 0 and len(CVEs) > 0):
+                vuln = Vulnerability("vul%d" % vuln_id, name, severity, port, risk_factor, CVEs, 0.5, 0)
+                vuln_id += 1
                 vulnerabilities.append(vuln)
+                if not port in uniq_ports:
+                    uniq_ports.append(port)
 print '[' + ', '.join(map(str, vulnerabilities)) + ']'
+
+for i in range(0, len(vulnerabilities)):
+    vuln = vulnerabilities[i]
+
+fixes = []
+goal = [[hosts[len(hosts) - 1]]]
+
+with open(args.domain, "w") as f:
+    f.write(str(PDDLDomain("coresec", vulnerabilities, goal, uniq_ports, fixes,
+                               "Bla",
+                               apply_once=not args.disable_apply_once)))
+
+with open(args.problem, "w") as f:
+    f.write(str(PDDLProblem("coresec", "coresec", hosts[0], goal,
+                                "Bla")))
+
+
 
