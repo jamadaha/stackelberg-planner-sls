c7937d740cb11932211632c0f30d1c9b54895107
Patrick Speicher <s9pkspei@stud.uni-saarland.de>
2017-01-26 11:29:11 +0100

    renamed folder and restructured files

--- OVAL_toolchain/what-if-analysis.py
+++
@@ -1,9 +0,0 @@
-import sys
-from subprocess import call, Popen, PIPE
-
-call(['python', 'parse_nessus_vulnerabilities.py'] + sys.argv[1:])
-p = Popen(['./plan.py', 'problem.pddl',  '--heuristic','h1=attack_success_prob_reuse(default_heuristic=blind)', '--heuristic', 'h2=lmcut(cost_type=3)', '--heuristic', 'h3=budget_dead_end(budget_heuristic=h2, prob_cost_heuristic=h1)', '--search', 'fixsearch(search_engine=astar(h3), attack_heuristic=h3, initial_attack_budget=40, initial_fix_budget=100000)'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-output, err = p.communicate()
-index1 = output.find("Resulting Pareto-frontier:")
-index2 = output.find("\nEND Pareto-frontier")
-print output[index1:index2]
