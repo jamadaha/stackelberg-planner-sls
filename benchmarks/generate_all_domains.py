aa94378e8679dc2adec8b0b509905de7a57064f6
Patrick Speicher <s9pkspei@stud.uni-saarland.de>
2017-08-18 19:55:43 +0200

    wrote script to generate all domains with all desired number of connections at once

---
+++ benchmarks/generate_all_domains.py
@@ -0,0 +1,16 @@
+import re
+import sys
+import random
+import argparse
+import os
+from shutil import copyfile
+
+domains_array = ["logistics98-robustness", "no-mystery-robustness-driving", "rovers-robustness-driving", "tpp-robustness-driving", "transport-opt11-strips-robustness-driving", "transport-opt14-strips-robustness-driving", "visitall-opt14-strips-robustness-driving"]
+number_of_cons_array = ["0", "1", "2", "4", "6", "8", "10", "12", "16", "32", "64", "128", "256", "512", "1024", "2048", "4096", "8192", "16384"]
+
+for domain in domains_array:
+    for i, cons in enumerate(number_of_cons_array):
+        if i== 0:
+            continue
+        #print "python modify-problems.py --dir " + domain + " --con-total " + cons + " --at-least-con " + number_of_cons_array[i-1]
+        os.system("python modify-problems.py --dir " + domain + " --con-total " + cons + " --at-least-con " + number_of_cons_array[i-1])
