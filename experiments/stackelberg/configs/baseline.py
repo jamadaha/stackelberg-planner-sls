2772be6f878813e3db8e5b5af7370eb64533b45c
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-04-10 00:45:41 +0200

    Modified experiments

--- experiments/stackelberg/configs/baseline.py
+++ experiments/stackelberg/configs/baseline.py
@@ -4,9 +4,15 @@ import sys
 
 from collections import defaultdict
 
-REVISION = "a4dd9da4d0af1b1134dcf2213b963e15415fc848"
-SERVERS = "old_servers" 
+REVISION = "95c3fd1292889ce5c50fdba75c5ba3b84cf5dd1c"
+SERVERS = "new_servers" 
 
 CONFIGS = defaultdict(list)
 
-CONFIGS["baseline"].append(configs.Config('baseline-lmcut', 'baseline-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "fixsearch(search_engine=astar(max([h1,h2]), pruning=null), attack_heuristic=h2, initial_attack_budget=2147483647, initial_fix_budget=2147483647, attack_op_dom_pruning=false, sort_fix_ops=true, greedy=false, upper_bound_pruning=true)"], REVISION, SERVERS))
+CONFIGS["baseline"] += [configs.Config('original-lmcut', 'original-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "fixsearch(search_engine=astar(max([h1,h2]), pruning=null), attack_heuristic=h2, initial_attack_budget=2147483647, initial_fix_budget=2147483647, attack_op_dom_pruning=false, sort_fix_ops=true, greedy=false, upper_bound_pruning=true)"], REVISION, SERVERS),
+                        configs.Config('baseline-lmcut', 'baseline-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "stackelberg(search_engine=astar(max([h1,h2])), follower_heuristic=h2)"], REVISION, SERVERS),
+                        configs.Config('baseline-sbd', 'baseline-sbd', ["--search", "stackelberg(search_engine=sbd())"], REVISION, SERVERS)
+
+]
+
+
