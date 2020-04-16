cf383b4ea9d66f48fc64346fa206b8a8d011ee78
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-04-16 12:04:42 +0200

    Fixed lab parser

--- lab_parser.py
+++ lab_parser.py
@@ -2,9 +2,12 @@
 
 from lab.parser import Parser
 from collections import defaultdict
+from ast import literal_eval
+
 import re
 
 eval = Parser()
+
 
 
 
@@ -12,15 +15,20 @@ regexps = [re.compile("Optimally solved follower subproblems: (?P<optimally_solv
            re.compile("Pareto-frontier: (?P<pareto_frontier>(.*))"),
            re.compile("Pareto-frontier size: (?P<pareto_frontier_size>(\d+))"),
            re.compile("Follower search time: (?P<follower_time>(.*))s"), 
-           re.compile("Leader search time: (?P<leader_time>(.*))"), 
+           re.compile("Leader search time: (?P<leader_time>(.*))s"), 
 ]
 
-# Search time: 29.3736s
-# Total time: 29.9618s
- 
+def parse_pareto_frontier(x):
+    x = x.strip()
+    if " " in x:
+        return map(literal_eval, x.split(" "))
+    else:
+        return [literal_eval(x)]
+
 type_atr = {'follower_time' : lambda x : max(0.01, float(x)),
             'leader_time' : lambda x : max(0.01, float(x)),
-            'pareto_frontier' : str,
+            'pareto_frontier' : parse_pareto_frontier,
+            'pareto_frontier_size' : int,
             "optimally_solved_subproblems" : int
         }
 
@@ -34,6 +42,14 @@ def parse_regexps (content, props):
                     props[item] = type_atr[item](data[item])
                 break
             
-eval.add_function(parse_regexps) 
+eval.add_function(parse_regexps)
+
+def set_coverage (content, props):
+    if 'pareto_front' in props:
+        props['coverage'] = 1
+        
+        
+eval.add_function(set_coverage) 
 
 
+eval.parse()
