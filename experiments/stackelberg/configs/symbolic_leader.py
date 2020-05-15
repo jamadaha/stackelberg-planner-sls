1ca1135784b8c62cd008670b51ff66e6d4d356c2
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-05-15 16:23:13 +0200

    New config for experiments

--- experiments/stackelberg/configs/symbolic_leader.py
+++ experiments/stackelberg/configs/symbolic_leader.py
@@ -4,12 +4,13 @@ import sys
 
 from collections import defaultdict
 
-REVISION = "f999d1b1ec7a1c7b5e4494054857859fb55e41ed"
+REVISION = "9e46b2ee32008462627cf810141dd06e6786856e"
 SERVERS = "all_servers" 
 
 CONFIGS = defaultdict(list)
 
-CONFIGS["symbolic_leader"] += [configs.Config('ss-sbd', 'ss-sbd', ["--search", "sym_stackelberg(optimal_engine=symbolic())"], REVISION, SERVERS)
+CONFIGS["symbolic_leader"] += [configs.Config('ss-sbd', 'ss-sbd', ["--search", "sym_stackelberg(optimal_engine=symbolic())"], REVISION, SERVERS),
+                               configs.Config('ss-sbd-origorder', 'ss-sbd-origorder', ["--search", "sym_stackelberg(optimal_engine=symbolic(stackelberg_variable_order=false))"], REVISION, SERVERS)
 ]
 
 
