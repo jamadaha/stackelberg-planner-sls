4872f44614934d22da25a2e2cb5d5a2d43001eaa
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-05-20 12:07:36 +0200

    Added lmcut configuration

--- experiments/stackelberg/configs/symbolic_leader.py
+++ experiments/stackelberg/configs/symbolic_leader.py
@@ -4,12 +4,13 @@ import sys
 
 from collections import defaultdict
 
-REVISION = "9a79cb7c1a6ebce32404de8749f67e2e32c17509"
+REVISION = "1a149cd24770af8587ce49d968ff298c721c9eb7"
 SERVERS = "all_servers" 
 
 CONFIGS = defaultdict(list)
 
 CONFIGS["symbolic_leader"] += [configs.Config('ss-sbd', 'ss-sbd', ["--search", "sym_stackelberg(optimal_engine=symbolic())"], REVISION, SERVERS),
+                               configs.Config('ss-lmcut', 'ss-lmcut', ["--search", "sym_stackelberg(optimal_engine=explicit(search_engine=astar(lmcut())))"], REVISION, SERVERS),
                                configs.Config('ss-sbd-origorder', 'ss-sbd-origorder', ["--search", "sym_stackelberg(optimal_engine=symbolic, stackelberg_variable_order=false)"], REVISION, SERVERS)
 ]
 
