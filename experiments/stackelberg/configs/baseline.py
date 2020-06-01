f8a7c75c5a3c3451d161ca2b162495bbc6b0c21d
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-06-01 11:28:19 +0200

    Changed revision for experiments

--- experiments/stackelberg/configs/baseline.py
+++ experiments/stackelberg/configs/baseline.py
@@ -5,13 +5,14 @@ import sys
 from collections import defaultdict
 
 REVISION = "a5b3f7d215c3eaffb2b9337653b39fbc96b6b050"
-SERVERS = "all_servers" 
+SERVERS = "new_servers" 
 
 CONFIGS = defaultdict(list)
 
-CONFIGS["baseline"] += [configs.Config('original-lmcut', 'original-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "fixsearch(search_engine=astar(max([h1,h2]), pruning=null), attack_heuristic=h2, initial_attack_budget=2147483647, initial_fix_budget=2147483647, attack_op_dom_pruning=false, sort_fix_ops=true, greedy=false, upper_bound_pruning=true)"], REVISION, SERVERS),
-                        configs.Config('baseline-lmcut', 'baseline-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "stackelberg(search_engine=astar(max([h1,h2])), follower_heuristic=h2)"], REVISION, SERVERS),
-                        configs.Config('baseline-sbd', 'baseline-sbd', ["--search", "stackelberg(search_engine=sbd())"], REVISION, SERVERS)
+CONFIGS["baseline"] += [
+    configs.Config('original-lmcut', 'original-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "fixsearch(search_engine=astar(max([h1,h2]), pruning=null), attack_heuristic=h2, initial_attack_budget=2147483647, initial_fix_budget=2147483647, attack_op_dom_pruning=false, sort_fix_ops=true, greedy=false, upper_bound_pruning=true)"], REVISION, SERVERS),
+    configs.Config('baseline-lmcut', 'baseline-lmcut', ["--heuristic", "h1=deadpdbs(max_time=120)", "--heuristic", "h2=lmcut", "--search", "stackelberg(search_engine=astar(max([h1,h2])), follower_heuristic=h2)"], REVISION, SERVERS),
+    configs.Config('baseline-sbd', 'baseline-sbd', ["--search", "stackelberg(search_engine=sbd())"], REVISION, SERVERS)
 
 ]
 
--- experiments/stackelberg/configs/symbolic_leader.py
+++ experiments/stackelberg/configs/symbolic_leader.py
@@ -4,7 +4,7 @@ import sys
 
 from collections import defaultdict
 
-REVISION = "8afa96ef3ce86a6b39ea031ac396f7b732dc43a5"
+REVISION = "a5b3f7d215c3eaffb2b9337653b39fbc96b6b050"
 SERVERS = "new_servers" 
 
 CONFIGS = defaultdict(list)
@@ -14,7 +14,7 @@ CONFIGS["symbolic_leader"] += [configs.Config('ss-sbd', 'ss-sbd', \
  configs.Config('ss-lmcut', 'ss-lmcut', ["--search", "sym_stackelberg(optimal_engine=explicit(search_engine=astar(lmcut())))"], REVISION, SERVERS),
  configs.Config('ss-sbd-up', 'ss-sbd-up', ["--search", "sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=false, plan_reuse_upper_bound=false), upper_bound_pruning=true)"], REVISION, SERVERS), 
  configs.Config('ss-sbd-ubreuse', 'ss-sbd-ubreuse', ["--search", "sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=false, plan_reuse_upper_bound=true), upper_bound_pruning=false)"], REVISION, SERVERS), 
- configs.Config('ss-sbd-up-ubreuse', 'ss-sbd-up-ubreuse', ["--search", "sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=true, plan_reuse_upper_bound=true), upper_bound_pruning=true)"], REVISION, SERVERS),
+ # configs.Config('ss-sbd-up-ubreuse', 'ss-sbd-up-ubreuse', ["--search", "sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=true, plan_reuse_upper_bound=true), upper_bound_pruning=true)"], REVISION, SERVERS),
  configs.Config('ss-sbd-up-ubreuse-tlim', 'ss-sbd-up-ubreuse-tlim', ["--search", "sym_stackelberg(optimal_engine=symbolic(plan_reuse_minimal_task_upper_bound=true, plan_reuse_upper_bound=true, force_bw_search_minimum_task_seconds=30, time_limit_seconds_minimum_task=300), upper_bound_pruning=true)"], REVISION, SERVERS),
 ]
 
