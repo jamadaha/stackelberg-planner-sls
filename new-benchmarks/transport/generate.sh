fb45d7096af14367bb7ae684df8be01eb10af94f
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-06-12 18:19:51 +0200

    transport added

---
+++ new-benchmarks/transport/generate.sh
@@ -0,0 +1,11 @@
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/two-cities-generator.py 10 1000 5 100 2 3 42 > p-2-3-10.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/two-cities-generator.py 14 1000 5 100 2 3 42 > p-2-3-14.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/two-cities-generator.py 18 1000 5 100 2 3 42 > p-2-3-18.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/two-cities-generator.py 22 1000 5 100 2 3 42 > p-2-3-22.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/two-cities-generator.py 26 1000 5 100 2 3 42 > p-2-3-26.pddl
+
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/city-generator.py 10 1000 5 100 2 4 42 > p-1-4-10.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/city-generator.py 14 1000 5 100 2 4 42 > p-1-4-14.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/city-generator.py 18 1000 5 100 2 4 42 > p-1-4-18.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/city-generator.py 22 1000 5 100 2 4 42 > p-1-4-22.pddl
+/home/alvaro/projects/benchmarks/code/pddl-generators/transport/city-generator.py 26 1000 5 100 2 4 42 > p-1-4-26.pddl
