9fe1e206794f5127000d62044fa8ffdd24718dd7
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-03-04 12:01:18 +0100

    feat: initial state exporer

---
+++ src/search/stackelberg/state_explorer.cc
@@ -0,0 +1,158 @@
+#include "state_explorer.h"
+#include "util.h"
+#include "../option_parser.h"
+#include "../plugin.h"
+#include <filesystem>
+#include <vector>
+#include "../symbolic/sym_controller.h"
+#include "../symbolic/uniform_cost_search.h"
+#include "symbolic_stackelberg_manager.h"
+#include "../utils/timer.h"
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include "follower_search_engine.h"
+#include "follower_task.h"
+#include "plan_reuse.h"
+#include "../symbolic/sym_variables.h"
+
+using namespace std;
+using namespace symbolic;
+
+namespace {
+    SearchEngine *parse_ss(OptionParser &parser) {
+        SearchEngine::add_options_to_parser(parser);
+        stackelberg::SymbolicStackelbergManager::add_options_to_parser(parser);
+        SymParamsSearch::add_options_to_parser(parser, 30e3, 10e7);
+
+        parser.add_option<bool>("upper_bound_pruning", "Upper bound pruning.",
+                                "true");
+        parser.add_option<bool>("follower_info",
+                                "Print additional info about follower searches",
+                                "false");
+        parser.add_option<int>(
+                "min_relevant_follower_cost",
+                "Any cost lower than this is irrelevant for the follower", "-1");
+
+        parser.add_option<stackelberg::PlanReuse *>("plan_reuse", "strategy for plan reuse",
+                                       "simple");
+        parser.add_option<stackelberg::FollowerSearchEngine *>("optimal_engine");
+        parser.add_option<stackelberg::FollowerSearchEngine *>("cost_bounded_engine", "", "",
+                                                  OptionFlags(false));
+
+        // Used for replacement plans
+        parser.add_option<std::string>(
+                "replacement_dir",
+                "Path to a directory in which follower plans are exported",
+                "replacements", OptionFlags(false));
+
+        parser.add_option<std::string>(
+                "replacement_title",
+                "What the directory replacements are added to should be called "
+                "as a sub-directory of \"replacement_dir\"",
+                ".", OptionFlags(false));
+        Options opts = parser.parse();
+        if (!parser.dry_run()) {
+            return new stackelberg::StateExplorer(opts);
+        }
+        return NULL;
+    }
+
+    Plugin<SearchEngine> state_explore("state_explore", parse_ss);
+}
+
+namespace stackelberg {
+    StateExplorer::StateExplorer(const Options &opts) :
+    SearchEngine(opts),
+    optimal_engine(opts.get<FollowerSearchEngine *>("optimal_engine")),
+    plan_reuse(opts.get<PlanReuse *>("plan_reuse")), mgrParams(opts),
+    searchParams(opts),
+    upper_bound_pruning(opts.get<bool>("upper_bound_pruning")),
+    statistics(opts.get<bool>("follower_info")),
+    min_relevant_follower_cost(opts.get<int>("min_relevant_follower_cost")) {
+        task = make_unique<StackelbergTask>();
+        stackelberg_mgr = std::make_shared<SymbolicStackelbergManager>(task.get(), opts);
+        optimal_engine->initialize(task.get(), stackelberg_mgr);
+
+        plan_reuse->initialize(stackelberg_mgr);
+
+        if (opts.contains("cost_bounded_engine")) {
+            cost_bounded_engine.reset(
+                    opts.get<FollowerSearchEngine *>("cost_bounded_engine"));
+            cost_bounded_engine->initialize(task.get(), stackelberg_mgr);
+        }
+    }
+
+    void StateExplorer::initialize() {
+        cout << "Initializing State Explore..." << endl;
+        auto t1 = chrono::high_resolution_clock::now();
+
+        vars = stackelberg_mgr->get_sym_vars();
+
+        leader_search_controller =
+                make_unique<SymController>(vars, mgrParams, searchParams);
+        leader_search = make_unique<UniformCostSearch>(leader_search_controller.get(),
+                                                       searchParams);
+        auto mgr = stackelberg_mgr->get_leader_manager();
+
+        leader_search->init(mgr, true);
+
+        statistics.stackelberg_search_initialized(t1);
+    }
+
+    SearchStatus StateExplorer::step() {
+        while (!leader_search->finished() && leader_search->getG() < 5)
+            leader_search->step();
+        BDD leader_states = leader_search->getClosedTotal();
+
+        BDD follower_initial_states =
+                stackelberg_mgr->get_follower_initial_state_projection(leader_states) *
+                stackelberg_mgr->get_static_follower_initial_state();
+
+        follower_initial_states =
+                plan_reuse->find_plan_follower_initial_states(follower_initial_states);
+
+
+        BDD bdd_valid;
+        BDD bdd_invalid;
+        size_t i = 0;
+        size_t valid = 0;;
+        size_t invalid = 0;
+        while (!follower_initial_states.IsZero() && i < 1000) {
+            auto state = stackelberg_mgr->sample_follower_initial_state(
+                    follower_initial_states);
+            auto solution = optimal_engine->solve(state, plan_reuse.get(),
+                                             std::numeric_limits<int>::max());
+            if (solution.has_plan() || solution.solution_cost() == 0) {
+                valid++;
+                if (i == 0)
+                    bdd_valid = vars->getStateBDD(state);
+                else
+                    bdd_valid *= vars->getStateBDD(state);
+
+            } else {
+                invalid++;
+                if (i == 0)
+                    bdd_invalid = vars->getStateBDD(state);
+                else
+                    bdd_invalid *= vars->getStateBDD(state);
+            }
+            follower_initial_states =
+                    plan_reuse->regress_plan_to_follower_initial_states(
+                            solution, follower_initial_states);
+            if (i++ % 10 == 0) {
+                double num_follower_initial_states = vars->numStates(
+                        follower_initial_states, stackelberg_mgr->get_num_follower_bdd_vars());
+                cout << "Follower states: " << num_follower_initial_states << endl;
+            }
+        }
+        cout << "Valid: " << valid << endl;
+        cout << "Invalid: " << invalid << endl;
+        exit(0);
+    }
+
+}
---
+++ src/search/stackelberg/state_explorer.h
@@ -0,0 +1,70 @@
+#ifndef SEARCH_STATE_EXPLORER_H
+#define SEARCH_STATE_EXPLORER_H
+
+#include <filesystem>
+#include <limits>
+#include <memory>
+#include <unordered_set>
+
+#include "stackelberg_statistics.h"
+
+#include "pareto_frontier.h"
+#include "stackelberg_task.h"
+
+#include "../attack_success_prob_reuse_heuristic.h"
+#include "../search_engine.h"
+#include "../successor_generator.h"
+#include "../symbolic/sym_enums.h"
+#include "../symbolic/sym_state_space_manager.h"
+
+#include "../symbolic/sym_params_search.h"
+
+
+namespace symbolic {
+    class UniformCostSearch;
+    class SymVariables;
+    class SymController;
+} // namespace symbolic
+
+namespace stackelberg {
+    class PlanReuse;
+    class FollowerSearchEngine;
+    class SymbolicStackelbergManager;
+    class StateExplorer : public SearchEngine {
+    public:
+        explicit StateExplorer(const Options &opts);
+
+        ~StateExplorer() override = default;
+
+    protected:
+        std::unique_ptr<StackelbergTask> task;
+        std::shared_ptr<SymbolicStackelbergManager> stackelberg_mgr;
+
+        std::unique_ptr<FollowerSearchEngine> optimal_engine;
+        std::unique_ptr<FollowerSearchEngine> cost_bounded_engine;
+
+        std::unique_ptr<PlanReuse> plan_reuse;
+
+        std::shared_ptr<symbolic::SymVariables> vars;
+        std::unique_ptr<symbolic::SymController> leader_search_controller;
+        std::unique_ptr<symbolic::UniformCostSearch> leader_search;
+        symbolic::SymParamsMgr
+                mgrParams; // Parameters for SymStateSpaceManager configuration.
+        symbolic::SymParamsSearch
+                searchParams; // Parameters to search the original state space
+
+        const bool upper_bound_pruning;
+
+        ParetoFrontier pareto_frontier;
+
+        StackelbergStatistics statistics;
+
+        const int min_relevant_follower_cost;
+
+
+        void initialize() final;
+
+        SearchStatus step() final;
+    };
+}
+#endif //SEARCH_STATE_EXPLORER_H
